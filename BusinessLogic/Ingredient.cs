f9848026152bc21dd7a17c89192be604e9bc450c
BeautifulLie <ladodayulia@mail.ru>
2017-05-02 20:26:30 +0300

    Add files via upload

---
+++ BusinessLogic/Ingredient.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace BusinessLogic
+{
+	public class Ingredient : IIngredient
+	{
+		public string Name { get; set; }
+		public double Price { get; set; }
+
+		public Ingredient()
+		{
+			// Need empty constructor for valid serializer work
+			Name = "";
+			Price = 0;
+		}
+
+		public Ingredient (string name, double price)
+		{
+			Name = name;
+			Price = price;
+		}
+
+		public override string ToString ()
+		{
+			return string.Format ("{0} ({1})", Name, Price);
+		}
+	}
+}
+
